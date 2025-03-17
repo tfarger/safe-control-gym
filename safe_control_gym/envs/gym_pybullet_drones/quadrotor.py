f428832b752149589f6cf412630aa6917c1f393d
Mingxuan Che <mxche2019@outlook.com>
2025-03-17 12:14:05 +0100

    minor change in quad default symbolic model value

--- safe_control_gym/envs/gym_pybullet_drones/quadrotor.py
+++ safe_control_gym/envs/gym_pybullet_drones/quadrotor.py
@@ -813,7 +813,7 @@ class Quadrotor(BaseAviary):
             # since they are specific to the 2D attitude model
             self.beta_1 = prior_prop.get('beta_1', 18.112984649321753)
             self.beta_2 = prior_prop.get('beta_2', 3.6800)
-            self.beta_3 = prior_prop.get('beta_3', 0)
+            self.beta_3 = prior_prop.get('beta_3', -0.008)
             self.alpha_1 = prior_prop.get('alpha_1', -140.8)
             self.alpha_2 = prior_prop.get('alpha_2', -13.4)
             self.alpha_3 = prior_prop.get('alpha_3', 124.8)
