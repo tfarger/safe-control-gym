a0c7dff6f52dbe253c35c028f1cc70d3fff9d586
Mingxuan Che <mxche2019@outlook.com>
2025-03-18 13:04:23 +0100

    minor code clean up

--- benchmarking_sim/quadrotor/mb_experiment_rollout.py
+++ benchmarking_sim/quadrotor/mb_experiment_rollout.py
@@ -125,10 +125,6 @@ def run(gui=False, n_episodes=1, n_steps=None, save_data=True,
     if ALGO in ['gpmpc_acados', 'gp_mpc', 'gpmpc_acados_TP', 'gpmpc_acados_TRP']:
         config.algo_config.gp_model_path = gp_model_dirs[seed-1]
     
-    # # remove process noise if there is any
-    # config.task_config.disturbances.action[0]['std'] = 0.0000 \
-    #     if eval_task not in ['proc_noise'] \
-    #     else config.task_config.disturbances.action[0]['std']
     # amplify the observation noise std with a factor 
     if eval_task == 'obs_noise':
         default_noise_std = config.task_config.disturbances.observation[0]['std']
@@ -138,7 +134,7 @@ def run(gui=False, n_episodes=1, n_steps=None, save_data=True,
     elif eval_task == 'proc_noise':
         default_noise_std = config.task_config.disturbances.action[0]['std']
         print(f'Original process noise std: {default_noise_std}')
-        config.task_config.disturbances.action[0]['std'] = noise_factor * default_noise_std 
+        config.task_config.disturbances.action[0]['std'] = [noise_factor * default_noise_std[i] for i in range(len(default_noise_std))]
         print(f'Amplified process noise std: {config.task_config.disturbances.action[0]["std"]}')
     # downwash height scale
     elif eval_task == 'downwash':
