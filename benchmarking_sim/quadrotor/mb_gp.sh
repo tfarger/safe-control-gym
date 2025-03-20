279e9a154730043df5aef2624f880f00c9f556ec
Mingxuan Che <mxche2019@outlook.com>
2025-03-20 16:44:47 +0100

    update gp train script

--- benchmarking_sim/quadrotor/mb_gp.sh
+++ benchmarking_sim/quadrotor/mb_gp.sh
@@ -1,32 +1,42 @@
 
-
+GP_BASE_TAG='100_200'
 # for RAND_TYPE in '_dw_h=1dot5' '_dw_h=2' '_dw_h=2dot5' '_dw_h=3'
 # for RAND_TYPE in '_ob_ns=5' '_ob_ns=15' '_ob_ns=25'  
 # for RAND_TYPE in  '_proc_ns=25' # '_proc_ns=5' '_proc_ns=15'
 # for RAND_TYPE in '_tr'
+
+# get the time
+START_TIME=$(date +%s)
+
+# for RAND_TYPE in '_ob_ns=5_proc_ns=3' \
+#                  '_ob_ns=10_proc_ns=5' \
+#                  '_ob_ns=15_proc_ns=7' \
+#                  '_ob_ns=5' '_ob_ns=10' '_ob_ns=15' \
+#                  '_proc_ns=3' '_proc_ns=5' '_proc_ns=7' 
+                #  '_ob_ns=20_proc_ns=10' \
+                #  '_ob_ns=20' \
+                #  '_proc_ns=10' 
 for RAND_TYPE in ''
 do
-    for STARTSEED in 1
-    do 
-        for algo in 'gpmpc_acados_TP'
-        do
-            python3 gpmpc_experiment.py $algo $RAND_TYPE
-        done
+    GP_TAG=$GP_BASE_TAG$RAND_TYPE
+    for algo in 'gpmpc_acados_TP'
+    do
+        python3 parallel_gpmpc_experiment.py $algo $RAND_TYPE
     done
 
-    # python3 results_dw.py 'gpmpc_acados_TP' $RAND_TYPE
-    # python3 results_noise.py 'gpmpc_acados_TP' 'obs_noise' $RAND_TYPE
-    # python3 results_noise.py 'gpmpc_acados_TP' 'proc_noise' $RAND_TYPE 
+    # python3 results_dw.py 'gpmpc_acados_TP' $GP_TAG
+    python3 results_noise.py 'gpmpc_acados_TP' 'obs_noise' $GP_TAG
+    python3 results_noise.py 'gpmpc_acados_TP' 'proc_noise' $GP_TAG
     # for ADDITIOANL in '_9' '_10' '_11' '_12' '_13' '_14' '_15'
     # do
-    #     for STARTSEED in 1 11 21 31 41 # 51 61 71 81 91 
-    #     do 
-    #         for algo in 'gpmpc_acados_TP'
-    #         do
-    #             python3 results_rollout.py $ADDITIOANL $STARTSEED $algo $RAND_TYPE
-    #         done
+    #     for algo in 'gpmpc_acados_TP'
+    #     do
+    #         python3 results_rollout.py $ADDITIOANL $STARTSEED $algo $GP_TAG
     #     done
     # done
 done
 
 python3 ../del_acados_files.py
+END_TIME=$(date +%s)
+ELAPSED_TIME=$((END_TIME - START_TIME))
+echo "Elapsed time: $ELAPSED_TIME seconds"
